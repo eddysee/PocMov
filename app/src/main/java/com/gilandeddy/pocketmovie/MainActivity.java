0a8926b6f5a097c8b5523f5ac56e0b9b2b2f8d92
adnan <adnansee@gmail.com>
2018-05-25 18:59:39 +0200

    Share Menu item without Pocket link.

--- app/src/main/java/com/gilandeddy/pocketmovie/MainActivity.java
+++ app/src/main/java/com/gilandeddy/pocketmovie/MainActivity.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ActionProvider;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.json.JSONArray;
@@ -16,8 +19,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import android.view.Menu;
+import android.widget.ShareActionProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionProvider shareActionProvider;
 
 
     @Override
@@ -30,5 +37,35 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        getMenuInflater().inflate(R.menu.menu_main_tab,menu);
+        MenuItem sharePocket = menu.findItem(R.id.action_share);
+        shareActionProvider = MenuItemCompat.getActionProvider(sharePocket);
+        sharePocket.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem sharePocket) {
+                String message = new String("Wanna watch");
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT,message);
+                startActivity(shareIntent);
+
+
+                return true;
+            }
+        });
+
+
+
+        return true;
+    }
+
 }
+
